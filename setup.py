0f6f433f78b7f132d61bebc3f10b54e0b24def1d
Liam DeVoe <orionldevoe@gmail.com>
2022-07-21 18:37:49 -0400

    convert to proper package instead of requirements.txt

---
+++ setup.py
@@ -0,0 +1,28 @@
+from distutils.core import setup
+from setuptools import find_packages
+import re
+
+# https://stackoverflow.com/a/7071358
+VERSION = "Unknown"
+VERSION_RE = r"^__version__ = ['\"]([^'\"]*)['\"]"
+
+with open("snitchvis/version.py") as f:
+    match = re.search(VERSION_RE, f.read())
+    if match:
+        VERSION = match.group(1)
+    else:
+        raise RuntimeError("Unable to find version string in "
+            "snitchvis/version.py")
+
+setup(
+    name="snitchvis",
+    version=VERSION,
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "PyQt6"
+    ],
+    package_data={
+        "circlevis": ["resources/*"]
+    }
+)
