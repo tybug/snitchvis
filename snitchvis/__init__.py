e7f41a406f387ee3c8b3eaf85b66819620178029
Liam DeVoe <orionldevoe@gmail.com>
2022-07-24 18:07:40 -0400

    export Snitch

--- snitchvis/__init__.py
+++ snitchvis/__init__.py
@@ -1,9 +1,9 @@
 from snitchvis.visualizer import (Snitchvis, SnitchvisApp, parse_events,
     parse_snitches, SnitchVisRecord, create_users, Event,
-    InvalidEventException, snitches_from_events)
+    InvalidEventException, snitches_from_events, Snitch)
 
 __all__ = [
     "Snitchvis", "SnitchvisApp", "SnitchVisRecord", "parse_events",
     "parse_snitches", "create_users", "Event", "InvalidEventException",
-    "snitches_from_events"
+    "snitches_from_events", "Snitch"
 ]
